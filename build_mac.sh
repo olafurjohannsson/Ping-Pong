5cde6124d9b7b324c4a4194fc51fde3d94aa9e5a
Ólafur Jóhannsson <olafurjohannss@gmail.com>
2017-05-09 22:14:36 +0000

    res and remove unneeded stuff

--- build_mac.sh
+++ build_mac.sh
@@ -1,1 +1,1 @@
-g++ -I /usr/local/include/ Pong.cpp -L /usr/local/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+g++ -I /usr/local/include/ Pong.cpp -L /usr/local/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -o main
